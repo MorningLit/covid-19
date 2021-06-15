8b0d67a4ecc4045850134373d125a1dca687e7c8
MorningLit <ambroseliew1998@gmail.com>
2021-06-15 17:47:28 +0800

    First Commit

---
+++ src/components/CountryList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="!countries"><h1>Loading</h1></div>
+  <div
+    v-else
+    v-for="country in countries"
+    :key="country.name"
+    class="country-wrapper"
+  >
+    <div class="flag-wrapper">
+      <img
+        v-bind:src="
+          'https://www.countryflags.io/' + country.code + '/flat/64.png'
+        "
+      />
+    </div>
+    <a>
+      {{ country.name }}
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CountryList",
+    props: ["countries"],
+  };
+</script>
+
+<style>
+  .country-wrapper {
+    border: 2px solid black;
+    border-radius: 16px;
+    height: 20vh;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .flag-wrapper {
+    width: 64px;
+    margin: auto;
+  }
+  .country-wrapper a {
+    display: block;
+    width: 200px;
+    text-align: center;
+  }
+  .country-wrapper:hover {
+    background: #d9dddc;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/WorldStatistics.vue
@@ -0,0 +1,59 @@
+<template>
+  <div v-if="!worldStats"><h1>Loading</h1></div>
+  <div v-else class="wrapper">
+    <div class="title">
+      <h3>ACTIVE: {{ worldStats.active }}</h3>
+      <h1><b>COVID-19 Statistics</b></h1>
+      <h2>{{ worldStats.date }}</h2>
+    </div>
+    <div>
+      <div class="new-cases">
+        <h2 class="deaths">NEW DEATHS: {{ worldStats.new_deaths }}</h2>
+        <h2 class="confirmed">NEW CONFIRMED: {{ worldStats.new_confirmed }}</h2>
+        <h2 class="recovered">NEW RECOVERED: {{ worldStats.new_recovered }}</h2>
+      </div>
+      <div class="total-cases">
+        <h3 class="deaths">DEATHS: {{ worldStats.deaths }}</h3>
+        <h3 class="confirmed">CONFIRMED: {{ worldStats.confirmed }}</h3>
+        <h3 class="recovered">RECOVERED: {{ worldStats.recovered }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "WorldStatistics",
+    props: ["worldStats"],
+  };
+</script>
+
+<style>
+  .title {
+    display: flex;
+    justify-content: space-between;
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+  }
+  .new-cases {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .total-cases {
+    display: flex;
+    justify-content: space-around;
+  }
+  .deaths {
+    color: darkred;
+  }
+  .confirmed {
+    color: darkblue;
+  }
+  .recovered {
+    color: darkgreen;
+  }
+</style>
